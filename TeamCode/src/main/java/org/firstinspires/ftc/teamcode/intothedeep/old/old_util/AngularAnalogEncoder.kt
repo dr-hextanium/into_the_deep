7018cc0e27ed81cb44f943937378cf2d57fe162e
robopandas <schsdrobotics@gmail.com>
2024-07-02 16:10:41 -0400

    god

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/intothedeep/old/old_util/AngularAnalogEncoder.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/intothedeep/old/old_util/AngularAnalogEncoder.kt
@@ -5,7 +5,9 @@ import dev.frozenmilk.dairy.calcified.hardware.sensor.CalcifiedAnalogInput
 import dev.frozenmilk.dairy.core.util.supplier.numeric.unit.EnhancedUnitSupplier
 import dev.frozenmilk.util.units.angle.Angle
 import dev.frozenmilk.util.units.angle.AngleUnit
+import dev.frozenmilk.util.units.angle.deg
 import dev.frozenmilk.util.units.angle.wrappedDeg
+import dev.frozenmilk.util.units.distance.inches
 import java.util.function.Supplier
 import kotlin.math.abs
 
@@ -14,15 +16,16 @@ class AngularAnalogEncoder(port: Int) : Supplier<Angle> {
     private val analog: CalcifiedAnalogInput
 
     var inverted = false
-        private set(value) { field = value }
+        private set
     var offset = 0.wrappedDeg
-        private set(value) { field = value }
+        private set
 
     init {
         analog = Calcified.controlHub.getAnalogInput(port)
 
         encoder = EnhancedUnitSupplier({
-            val raw = (analog.supplier.get() / 3330.0)
+            val raw = (analog.supplier.get() / 3330.0) // in [0, 1]
+
             val inverted = if (inverted) abs(1.0 - raw) else raw
 
             (360.0 * inverted).wrappedDeg
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/intothedeep/zarif/Swerve.kt
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.intothedeep.zarif
+
+class Swerve {
+}
