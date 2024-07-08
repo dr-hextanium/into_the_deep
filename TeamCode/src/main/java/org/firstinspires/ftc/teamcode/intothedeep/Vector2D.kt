b527fe8f6019944a6344a1a118f784aeb96e6480
robopandas <schsdrobotics@gmail.com>
2024-07-08 13:33:03 -0400

    refactor: add `magnitude`, remove getter from `theta`, remove unneeded equality overload

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/intothedeep/Vector2D.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/intothedeep/Vector2D.kt
@@ -2,11 +2,12 @@ package org.firstinspires.ftc.teamcode.intothedeep
 
 import kotlin.math.atan2
 import kotlin.math.cos
+import kotlin.math.hypot
 import kotlin.math.sin
 
 data class Vector2D(val x: Double, val y: Double) {
-	val theta: Double
-		get() = atan2(y, x) * (180.0 / Math.PI)
+	val theta = atan2(y, x) * (180.0 / Math.PI)
+	val magnitude = hypot(x, y)
 
 	operator fun plus(other: Vector2D) = Vector2D(x + other.x, y + other.y)
 	operator fun minus(other: Vector2D) = Vector2D(x - other.x, y - other.y)
@@ -15,12 +16,6 @@ data class Vector2D(val x: Double, val y: Double) {
 	operator fun times(a: Double) = Vector2D(x * a, y * a)
 	operator fun unaryMinus() = Vector2D(-x, -y)
 	operator fun unaryPlus() = Vector2D(+x, +y)
-
-	override fun equals(other: Any?): Boolean {
-		return if(other is Vector2D) {
-			x == other.x && y == other.y && theta == other.theta
-		} else false
-	}
 
 	override fun toString() = "Vector2D(x = $x, y = $y, theta = $theta)"
 
