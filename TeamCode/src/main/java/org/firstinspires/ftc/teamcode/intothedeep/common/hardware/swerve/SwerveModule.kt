49919206c4ba95acb4408dd0093c3fb7718253d7
dr-hextanium <hextanium.on.discord@gmail.com>
2024-07-18 01:41:00 -0400

    add: pidf constants

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/intothedeep/common/hardware/swerve/SwerveModule.kt
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode.intothedeep.common.hardware.swerve
+
+import com.acmerobotics.dashboard.config.Config
+
+class SwerveModule {
+}
+
+@Config
+object ModuleConstants {
+    @JvmField
+    var kP = 0.0
+
+    @JvmField
+    var kI = 0.0
+
+    @JvmField
+    var kD = 0.0
+
+    @JvmField
+    var kS = 0.0
+}
