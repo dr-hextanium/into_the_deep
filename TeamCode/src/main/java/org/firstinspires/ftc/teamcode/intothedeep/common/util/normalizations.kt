cfa0640ea2e5ba469f53d31c76107866b830e8a2
dr-hextanium <hextanium.on.discord@gmail.com>
2024-07-18 02:17:37 -0400

    refactor: definition of `normalizations`

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/intothedeep/common/util/normalizations.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/intothedeep/common/util/normalizations.kt
@@ -1,8 +1,10 @@
 package org.firstinspires.ftc.teamcode.intothedeep.common.util
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit.normalizeDegrees
+
 
 // [0, 2π)
-fun norm(angle: Double) = angle % 360
+fun norm(angle: Double) = normDelta(angle) + 180.0
 
 // [-π, π)
-fun normDelta(angle: Double) = (angle + 180.0) % 360 - 180.0
+fun normDelta(angle: Double) = normalizeDegrees(angle)
